091f800324610e5c8ff6688b4f748b7d4fc828ba
senwix <kunykunykuny6@gmail.com>
2026-01-15 00:00:32 +0200

    Add files via upload

--- WMs/hyprland.nix
+++ WMs/hyprland.nix
@@ -9,14 +9,28 @@
   
   environment.systemPackages = with pkgs;
     [
+      brightnessctl
+      gsimplecal
       hyprcursor
       hyprland
       hyprland-protocols
       hyprland-qtutils
       hyprpaper
+      hyprpicker
       hyprshot
-      xdg-desktop-portal-hyprland
+      networkmanagerapplet
+      nerd-fonts.jetbrains-mono
+      pavucontrol
+      pulseaudioFull
+      slurp
+      waybar
+      wlogout
       wofi
     ];
+
+    xdg.portal = {
+      enable = true;
+      extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+    };
 
 }
--- WMs/plasma.nix
+++ WMs/plasma.nix
@@ -14,6 +14,9 @@
 
     environment.systemPackages = with pkgs.kdePackages;
       [
+        discover
+        dragon
+        kdenlive
         kwrited
       ];
 
