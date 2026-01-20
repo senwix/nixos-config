71d9c11d61883f9650b3c6d5a9ba60af10351ef7
senwix <kunykunykuny6@gmail.com>
2026-01-20 20:29:29 +0200

    Delete networking.nix

--- networking.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-
-{
-
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
-  networking.wireless.networks.echelon.pskRaw = "2c95e6ec261704570953851beae9a85716d9e607f6ca634339cbee21349b42e9";
-
-}
